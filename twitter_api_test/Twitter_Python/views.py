6a4821dbe2f5b9fac871f04ee7f05ba0ee1eabd8
unknown <ninadmsn@outlook.com>
2020-11-20 01:58:52 +0530

    connection made for templates

--- twitter_api_test/Twitter_Python/views.py
+++ twitter_api_test/Twitter_Python/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
+import tweepy
+from . import config
 # Create your views here.
+
+def indexPage(request):    
+    # return HttpResponse("Hello")
+    auth=tweepy.OAuthHandler(config.api_key,config.api_secret_key)
+    auth.set_access_token(config.token)
+    api = tweepy.API(auth)
+    
+    return render(request,'index.html')
+
+    
