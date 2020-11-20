4335bad164df08f5efc81ccfdfe4c5af17792e6e
unknown <ninadmsn@outlook.com>
2020-11-20 11:40:07 +0530

    fetching followers example added

--- twitter_api_test/Twitter_Python/views.py
+++ twitter_api_test/Twitter_Python/views.py
@@ -7,9 +7,18 @@ from . import config
 def indexPage(request):    
     # return HttpResponse("Hello")
     auth=tweepy.OAuthHandler(config.api_key,config.api_secret_key)
-    auth.set_access_token(config.token)
+    auth.set_access_token(config.access_token,config.token_secret)
     api = tweepy.API(auth)
     
-    return render(request,'index.html')
+    ids = api.followers_ids(screen_name='ninad_dadmal',count=30)
+    myFollowers=[]
+    for users in api.lookup_users(user_ids=ids):
+        print("https://twitter.com/{}".format(users.screen_name))
+        myFollowers.append(users.screen_name)
+    mydictonary={
+        "myFollowers":myFollowers,
+    }
+    # print(myFollowers)
+    return render(request,'index.html',context=mydictonary)
 
     
