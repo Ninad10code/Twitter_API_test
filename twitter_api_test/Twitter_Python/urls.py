c054de4cc5bfe4435cbbbbc30b486b5ffdca279c
unknown <ninadmsn@outlook.com>
2020-11-22 01:22:30 +0530

    fetching details based on hastag and fetching tweets of user ids

--- twitter_api_test/Twitter_Python/urls.py
+++ twitter_api_test/Twitter_Python/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.indexPage,name="indexPage"),
+    path('tweets_loc',views.tweets_loc,name="tweets_loc"),
+    path('particular',views.particular,name="particular"),
 ]
--- twitter_api_test/Twitter_Python/views.py
+++ twitter_api_test/Twitter_Python/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse,JsonResponse
 import tweepy
 from . import config
 # Create your views here.
+auth=tweepy.OAuthHandler(config.api_key,config.api_secret_key)
+auth.set_access_token(config.access_token,config.token_secret)
+api = tweepy.API(auth)
 
 def indexPage(request):    
     # return HttpResponse("Hello")
-    auth=tweepy.OAuthHandler(config.api_key,config.api_secret_key)
-    auth.set_access_token(config.access_token,config.token_secret)
-    api = tweepy.API(auth)
+    
     
     ids = api.followers_ids(screen_name='ninad_dadmal',count=30)
     myFollowers=[]
@@ -22,3 +23,41 @@ def indexPage(request):
     return render(request,'index.html',context=mydictonary)
 
     
+def tweets_loc(request):
+    """
+    docstring
+    """
+    search_word="#Codechella"+"-filter:retweets"
+    date_since="2020-11-19"
+    tweets=tweepy.Cursor(api.search,q=search_word,lang="en",since=date_since).items(10)
+    # tweets_collection=[tweet.text for tweet in tweets]
+    # username_collection=[tweet.user.screen_name for tweet in tweets]
+    # location_collection=[tweet.user.location for tweet in tweets]
+
+    # Collect a list of tweets
+    # [tweet.text for tweet in tweets]
+    collection=[[tweet.text,tweet.user.screen_name,tweet.user.location] for tweet in tweets]
+    # print(collection)
+    mydictionary={
+        # "tweets":tweets_collection,
+        "tweet_data":collection,
+        "range_list":3,
+    }
+
+    return render(request,'tweets_location.html',context=mydictionary)
+
+def particular(request):
+    # search_word="#Codechella"+"-filter:retweets"
+    user_id="Vedant_Bahel"
+    date_since="2020-11-19"
+    tweets=api.user_timeline(screen_name=user_id,count=3,include_rts=False,tweet_mode='extended')
+    collection=[[tweet.id,tweet.full_text,tweet.favorite_count] for tweet in tweets]
+    # print(collection)
+    mydictionary={
+        "tweet_info":collection,
+    }
+    return render(request,'particular.html',context=mydictionary)
+    # for info in collection[:2]:
+    #     print(info.id)
+    #     print(info.full_text)
+    #     print('\n')
